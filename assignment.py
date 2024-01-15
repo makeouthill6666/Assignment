0d999db880cae43fd2cffe7cc3444a52f26f0b32
makeouthill6666 <superduperrookie@gmail.com>
2024-01-15 17:59:43 +0900

    1.15 assignment

--- assignment.py
+++ assignment.py
@@ -1,8 +1,24 @@
 # (0°C × 9/5) + 32 = 32°F
 # (32°F − 32) × 5/9 = 0°C
 
-Menu = Input ('1) Celsius to Fahrenheit', '2) Fahrenheit to Celsius', '3) quit program')
-if menu == '1':
-    Celsius = float(input('Input Celsius : '))
-    print(f'(Celsius*9/5)+32'F)
-
+while True :
+    Menu = input("1) Celsius to Fahrenheit 2)Fahrenheit to Celsius 3) Quit : ")
+    print()
+    if Menu not in ['1', '2', '3'] :
+        print("Please choose from menu")
+    if Menu == '1' :
+        try :
+            Celsius = float(input('Input Celsius : '))
+            print(f'{Celsius}C, {(Celsius*9/5)+32:.4f}F')
+        except ValueError :
+            print("Invalid input. Please enter the number")
+    elif Menu == '2' :
+        try :
+            Fahrenheit = float(input('Input Fahrenheit : '))
+            print(f'{Fahrenheit}F, {(Fahrenheit-32)*5/9:.4f}C')
+        except ValueError :
+            print("Invalid input. Please enter the number")
+    elif Menu == '3' :
+        print('terminate program')
+        break
+    print()
