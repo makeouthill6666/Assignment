6da29b725fbe137a6e16d25aca70b43f36e914df
makeouthill6666 <superduperrookie@gmail.com>
2024-01-15 16:28:35 +0900

    v0.2 Assignment File

---
+++ assignment.py
@@ -0,0 +1,8 @@
+# (0°C × 9/5) + 32 = 32°F
+# (32°F − 32) × 5/9 = 0°C
+
+Menu = Input ('1) Celsius to Fahrenheit', '2) Fahrenheit to Celsius', '3) quit program')
+if menu == '1':
+    Celsius = float(input('Input Celsius : '))
+    print(f'(Celsius*9/5)+32'F)
+
